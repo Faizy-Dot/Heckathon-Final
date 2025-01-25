41425fb22e745483ae80f005ee44942cefc3c4a1
Faizy-Dot <160147462+Faizy-Dot@users.noreply.github.com>
2025-01-26 01:22:48 +0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Admin from "./admin/Admin";
-import ManageUsers from "./AdminComponents/ManageUser";
 import LandingPage from "./pages/landingPage/LandingPage";
+import AdminDashboard from "./admin/Admin";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
-         <Route path="/admin" element={<Admin />} />
-         <Route path="/admin/userManager" element={<ManageUsers />}/>
+         <Route path="/admindashboard" element={<AdminDashboard/>} />
         {/* <Route path="/receptionist" element={<ReceptionistDashboard />} /> */}
         {/* <Route path="/department" element={<DepartmentDashboard />} />  */}
       </Routes>
