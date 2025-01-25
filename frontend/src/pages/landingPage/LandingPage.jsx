d8c8707b3529ea500434b79775d6cd060cbdc730
Faizy-Dot <160147462+Faizy-Dot@users.noreply.github.com>
2025-01-26 01:01:22 +0500

    update

---
+++ frontend/src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./style.css"
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1 className='check'>LandingPage</h1>
+    </div>
+  );
+}
