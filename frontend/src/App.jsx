cc1ffca79964a41720cae2f086ddfc7487e426c1
Faizy-Dot <160147462+Faizy-Dot@users.noreply.github.com>
2025-01-26 02:12:10 +0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/landingPage/landingPage";
+import HomePage from "./pages/landingPage/LandingPage";
 
 
 export default function App() {
