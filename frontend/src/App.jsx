0d2a038d3e848f725fd103b01f807318c5bd5ec3
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2025-01-26 01:06:20 +0500

    Add Admin panell and work manage user components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Admin from "./admin/Admin";
+import ManageUsers from "./AdminComponents/ManageUser";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
-        {/* <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/receptionist" element={<ReceptionistDashboard />} />
-        <Route path="/department" element={<DepartmentDashboard />} /> */}
+         <Route path="/admin" element={<Admin />} />
+         <Route path="/admin/userManager" element={<ManageUsers />}/>
+        {/* <Route path="/receptionist" element={<ReceptionistDashboard />} /> */}
+        {/* <Route path="/department" element={<DepartmentDashboard />} />  */}
       </Routes>
     </Router>
   );
